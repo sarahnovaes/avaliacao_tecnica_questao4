1d93f30b3566f2cff290a380043fed54ad766a9b
sarahnovaes <118676911+sarahnovaes@users.noreply.github.com>
2022-11-20 12:22:54 -0300

    Add files via upload

---
+++ main4.js
@@ -0,0 +1,5 @@
+var nome = ['Fabiana Araújo', 'Gabriel Gomes', 'Fernando Henrique', 'Ana Luiza', 'Geralda do Nascimento', 'Miguel Souza', 'Antonio Miguel'];
+
+var idade = ['33', '25', '17', '2', '93', '70', '69'];
+
+console.log(nome);
